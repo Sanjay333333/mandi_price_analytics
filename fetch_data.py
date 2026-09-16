861042992a0c015f3d7d267f1a42b550f0b83865
Sanjay333333 <sanjay3336669999@gmail.com>
2026-09-16 16:08:48 +0530

    Fix API key loading by adding dotenv

--- fetch_data.py
+++ fetch_data.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def fetch_and_save():
     url = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070"
